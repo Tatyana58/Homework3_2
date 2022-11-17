1f177929ca99af3f201dfb10de15ee2a940ac4bc
Tatyana58 <tatbir@rambler.ru>
2022-11-17 20:14:49 +0300

    Beginning Task 1 Task 2 Task 3 Task 4 Task 5

--- src/Main.java
+++ src/Main.java
@@ -88,5 +88,43 @@ public class Main {
             System.out.println("Потребуется дней: " + (deliveryTime+2));
         }
         System.out.println(" ");
+
+        System.out.println("Задание 5");
+        //Задание 5
+        //Напишите программу, которая определяет по номеру месяца в году,
+        //к какому сезону этот месяц принадлежит. Например, 1 месяц (он же январь) принадлежит к сезону зима.
+        //Для написания программы используйте оператор switch.
+        // Для обозначения номера месяца используйте переменную monthNumber = 12.
+        //Пропишите условие, при котором программа не будет выполняться (номер месяца больше 13).
+        int monthNumber = 12;
+        switch (monthNumber) {
+            case 1: System.out.println("Зима");
+                break;
+            case 2: System.out.println("Зима");
+                break;
+            case 3: System.out.println("Весна");
+                break;
+            case 4: System.out.println("Весна");
+                break;
+            case 5: System.out.println("Весна");
+                break;
+            case 6: System.out.println("Лето");
+                break;
+            case 7: System.out.println("Лето");
+                break;
+            case 8: System.out.println("Лето");
+                break;
+            case 9: System.out.println("Осень");
+                break;
+            case 10: System.out.println("Осень");
+                break;
+            case 11: System.out.println("Осень");
+                break;
+            case 12: System.out.println("Зима");
+                break;
+            default:System.out.println("Такого месяца в году нет!");
+                break;
+        }
+        System.out.println(" ");
     }
 }
